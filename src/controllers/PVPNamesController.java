47bf47d4459f75d46b860693bc20b2ed4ae89277
Marwan <marwan.elbahnasawy@gmail.com>
2022-12-07 10:56:53 +0200

    added names for pvp and pc

---
+++ src/controllers/PVPNamesController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Copy Center
+ */
+public class PVPNamesController implements Initializable {
+
+    
+    
+    @FXML
+    private TextField PlayerXTxt;
+    @FXML
+    private TextField PlayerOTxt;
+    @FXML
+    private Button nextBtn;
+    @FXML
+    private ImageView backImg;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+ 
+    
+}
