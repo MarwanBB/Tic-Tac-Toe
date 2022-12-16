4f10d109d175e3f5245c20e44b64cea2c5b9c9b1
Marwan <marwan.elbahnasawy@gmail.com>
2022-12-16 21:45:12 +0200

    added_method_send_message_in_client

--- TicTacToeGame/src/controllers/Client.java
+++ TicTacToeGame/src/controllers/Client.java
@@ -1,51 +1,80 @@
+
 package controllers;
 
+import Utility.Constants;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import models.User;
+
 
 public class Client {
-
     Socket mySocket;
     PrintStream printstream;
-
-    public Client() {
-        try {
-            mySocket = new Socket("127.0.0.1", 5005);
-
+        //DataInputStream dis;
+        //PrintStream ps;
+        
+        
+        
+            public Client(){
+            try {
+            mySocket = new Socket("127.0.0.1" , 5005);
+            
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        createThread();
-    }
+            createThread();
+}
+        
+        
+        
+        public void createThread(){
+             
+             new Thread(){
+                 @Override
+                public void run() {
 
-    public void createThread() {
+                    while(true){
+                             DataInputStream dataInputStream;
+                             try {
+                                 dataInputStream = new DataInputStream(mySocket.getInputStream());
+                                 String str = dataInputStream.readLine();
 
-        new Thread() {
-            @Override
-            public void run() {
-
-                while (true) {
-                    DataInputStream dataInputStream;
-                    try {
-                        dataInputStream = new DataInputStream(mySocket.getInputStream());
-                        String str = dataInputStream.readLine();
-
-                        if (str != null) {
-                            if (!str.isEmpty()) {
-                            }
-                        }
-                    } catch (IOException e) {
-                        System.out.println(e.toString());
+                                 if(str!=null)
+                                         {
+                                             if(!str.isEmpty()){                            
+                                                 //textArea.appendText(str+"\n");
+                                             }
+                                         }
+                             } catch (IOException e) {
+                                 System.out.println(e.toString());
+                             }
                     }
                 }
-            }
-        }.start();
-    }
-
-    public void sendMessage() {
-
-    }
+             }.start();}
+        
+        public void sendMessage(User user) {
+        try {
+            
+            System.out.println(user.getUsername());
+            System.out.println(user.getPassword());
+            
+            System.out.println(printstream);
+            printstream = new PrintStream(mySocket.getOutputStream());
+            printstream.println(Constants.signUp + "/" + user.getUsername() + "/" + user.getPassword());
+            System.out.println(printstream);
+            
+            
+            //printStream.println();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+        }
+        
+        
 
 }
